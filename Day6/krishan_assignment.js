8f0ec19c9d995a55586aa7ba7452d06de8bdb1d1
Krishankant475 <kirshan5673347239474@gmail.com>
2025-11-06 13:52:07 +0530

    Create krishan_assignment.js

---
+++ Day6/krishan_assignment.js
@@ -0,0 +1,56 @@
+// Q.1
+
+// let arr = ["krishan", "kapil", "sandeep"];
+// console.log(arr);
+
+// arr.push("harish")
+// console.log( arr)
+
+// arr.shift("sandeep")
+// console.log(arr)
+
+// arr.indexOf("krishan")
+// console.log(arr)
+
+
+
+// Q.2
+
+// function krish(a, b){
+//     a * a + b * b;
+// }
+// console.log(krish(3,4));
+
+
+
+// Q3
+
+
+// const book = {
+//     title: "the gift of the magi",
+//     author: "O.henry",
+//     published: 1906,
+//     Details: function () {
+//         return `${this.title} by ${this.author}, published in ${this.published}.`;
+
+//     }
+
+// };
+// console.log(book.Details());
+
+
+
+// Q4
+
+// const mySet = new Set();
+
+// mySet.add("krish");
+// mySet.add("sandeep");
+// mySet.add("kapil");
+// // console.log(mySet);
+
+// mySet.delete("krish");
+// console.log(mySet)
+
+// mySet.has("krish");
+// console.log(mySet.has("krish"))
